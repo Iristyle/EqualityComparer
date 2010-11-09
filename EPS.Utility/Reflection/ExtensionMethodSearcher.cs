fc3272e31d953023eff7bb463edad52253ea0a45
ebrown <devnull@localhost>
2010-11-09 17:02:29 -0500

    cleaned up code updated a bunch of inline docs some initial commits

---
+++ EPS.Utility/Reflection/ExtensionMethodSearcher.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace EPS.Reflection
+{
+    /// <summary>
+    /// Provides a way to find all extensions methods in assemblies, extension methods for a given type, or
+    /// a way to filter the list of extensions methods based on MethodInfo.
+    /// </summary>
+    public static class ExtensionMethodSearcher
+    {
+        /// <summary>   Gets the extension methods available in all currently loaded assemblies within the AppDomain that apply to a given type. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <typeparam name="T">    The Type to inspect. </typeparam>
+        /// <returns>   A List&lt;MethodInfo&gt; with all the extension methods. </returns>
+        public static List<MethodInfo> GetExtensionMethodsInCurrentAssemblies<T>()
+        {
+            return GetExtensionMethodsInCurrentAssemblies(typeof(T));
+        }
+
+        /// <summary>   Gets the extension methods available in all currently loaded assemblies within the AppDomain that apply to a given type. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <param name="extendedType"> The Type being extended with extension methods. </param>
+        /// <returns>   A List&lt;MethodInfo&gt; with all the extension methods. </returns>
+        public static List<MethodInfo> GetExtensionMethodsInCurrentAssemblies(this Type extendedType)
+        {
+            if (null == extendedType)
+                throw new ArgumentNullException("extendedType");
+
+            List<MethodInfo> methods = new List<MethodInfo>();
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies().AsParallel())
+            {
+                lock (methods)
+                    methods.AddRange(GetExtensionMethods(assembly, extendedType));
+            }
+
+            return methods;
+        }
+
+        /// <summary>   Gets the extension methods available in all currently loaded assemblies within the AppDomain that apply to a given type. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <param name="extendedType"> The Type being extended with extension methods. </param>
+        /// <param name="selector">     The filtering function used to inspect the methods when building the list. </param>
+        /// <returns>   A List&lt;MethodInfo&gt; with all the extension methods. </returns>
+        public static List<MethodInfo> GetExtensionMethodsInCurrentAssemblies(this Type extendedType, Func<MethodInfo, bool> selector)
+        {
+            if (null == extendedType)
+                throw new ArgumentNullException("extendedType");
+            if (null == selector)
+                throw new ArgumentNullException("selector");
+
+            List<MethodInfo> methods = new List<MethodInfo>();
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies().AsParallel())
+            {
+                lock (methods)
+                    methods.AddRange(GetExtensionMethods(assembly, extendedType, selector));
+            }
+
+            return methods;
+        }
+
+        /// <summary>   Gets the extension methods available in a given assembly that apply to a given type. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <param name="assembly">     The given assembly. </param>
+        /// <param name="extendedType"> The Type being extended with extension methods. </param>
+        /// <returns>   A List&lt;MethodInfo&gt; with all the extension methods. </returns>
+        public static IEnumerable<MethodInfo> GetExtensionMethods(this Assembly assembly, Type extendedType)
+        {
+            if (null == assembly)
+                throw new ArgumentNullException("assembly");
+            if (null == extendedType)
+                throw new ArgumentNullException("extendedType");
+
+            var query = from type in assembly.GetTypes().AsParallel()
+                        where type.IsSealed && !type.IsGenericType && !type.IsNested
+                        from method in type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                        where method.IsDefined(typeof(ExtensionAttribute), false)
+                        where method.GetParameters()[0].ParameterType == extendedType
+                        select method;
+            return query;
+        }
+
+        /// <summary>   Gets the extension methods available in a given assembly that apply to a given type. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <param name="assembly">     The given assembly. </param>
+        /// <param name="extendedType"> The Type being extended with extension methods. </param>
+        /// <param name="selector">     The filtering function used to inspect the methods when building the list. </param>
+        /// <returns>   A List&lt;MethodInfo&gt; with all the extension methods. </returns>
+        public static IEnumerable<MethodInfo> GetExtensionMethods(this Assembly assembly, Type extendedType, Func<MethodInfo, bool> selector)
+        {
+            if (null == assembly)
+                throw new ArgumentNullException("assembly");
+            if (null == extendedType)
+                throw new ArgumentNullException("extendedType");
+            if (null == selector)
+                throw new ArgumentNullException("selector");
+
+            var query = from type in assembly.GetTypes().AsParallel()
+                        where type.IsSealed && !type.IsGenericType && !type.IsNested
+                        from method in type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                        where method.IsDefined(typeof(ExtensionAttribute), false)
+                        where method.GetParameters()[0].ParameterType == extendedType
+                        where selector.Invoke(method)
+                        select method;
+            return query;
+        } 
+    }
+}
---
+++ EPS.Utility/Reflection/TypeExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace EPS.Reflection
+{
+    /// <summary>   A set of useful extension methods built on top of <see cref="T:System.Type"/> </summary>
+    /// <remarks>   ebrown, 11/9/2010. </remarks>
+    public static class TypeExtensions
+    {
+        /// <summary>   Determines whether a given type implements a specified interface, where the interface *must* be generic. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <exception cref="ArgumentException">        Thrown when one or more arguments have unsupported or illegal values. </exception>
+        /// <param name="interfaceType">    The Type of the interface to search for. </param>
+        /// <param name="concreteType">     The concrete object Type to examine. </param>
+        /// <returns>   <c>true</c> if the concrete type specified implements the interface type specified; otherwise, <c>false</c>. </returns>
+        public static bool IsGenericInterfaceAssignableFrom(this Type interfaceType, Type concreteType)
+        {
+            if (null == interfaceType) throw new ArgumentNullException("interfaceType");
+            if (null == concreteType) throw new ArgumentNullException("concreteType");
+            
+            if (!interfaceType.IsGenericType || !interfaceType.IsInterface)
+                throw new ArgumentException("interfaceType must be a generic interface such as IInterface<T>");
+
+            return concreteType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
+        }
+
+        /// <summary>   
+        /// For a given type implementing a specified interface, where the interface *must* be generic, this returns the parameters passed to the
+        /// generic interface. 
+        /// </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <exception cref="ArgumentException">        Thrown when one or more arguments have unsupported or illegal values. </exception>
+        /// <param name="interfaceType">    The Type of the interface to search for. </param>
+        /// <param name="concreteType">     The concrete object Type to examine. </param>
+        /// <returns>   An enumeration of the Types being used in the generic interface declaration. </returns>
+        public static IEnumerable<Type> GetGenericInterfaceTypeParameters(this Type interfaceType, Type concreteType)
+        {
+            if (null == interfaceType) throw new ArgumentNullException("interfaceType");
+            if (null == concreteType) throw new ArgumentNullException("concreteType");
+
+            if (!interfaceType.IsGenericType || !interfaceType.IsInterface)
+                throw new ArgumentException("interfaceType must be a generic interface such as IInterface<T>");
+
+            var interfaces = concreteType.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType).ToList();
+            if (interfaces.Count == 0)
+                throw new ArgumentException(String.Format("Interface {0} not implemented by {1}", interfaceType, concreteType), "concreteType");
+
+            return interfaces[0].GetGenericArguments();
+        }
+
+        /// <summary>   Determines whether the specified type is anonymous. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        /// <param name="type"> The type. </param>
+        /// <returns>   <c>true</c> if the specified type is anonymous; otherwise, <c>false</c>. </returns>
+        public static bool IsAnonymous(this Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            // HACK: The only way to detect anonymous types right now.
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+                       && type.IsGenericType && type.Name.Contains("AnonymousType")
+                       && (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
+                       && (type.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
+        }
+
+        /// <summary>   Determines whether the specified object is anonymous. </summary>
+        /// <remarks>   ebrown, 11/9/2010. </remarks>
+        /// <param name="o">    The object to inspect. </param>
+        /// <returns>   <c>true</c> if the specified object is based on an anonymous type; otherwise, <c>false</c>. </returns>
+        public static bool IsAnonymous(this object o)
+        {
+            return IsAnonymous(o.GetType());
+        }
+    }
+}
