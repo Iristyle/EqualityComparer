d678458530cfd58d9ce451ebcbcda494a351b630
ebrown <devnull@localhost>
2010-11-24 16:40:18 -0500

    added in shared assembly versioning / msbuild script ripped assembly file version, company and copyright out of project assemblyinfocs and moved them to shared assemblyinfo cleaned up / refactored build scripts a little more added a bunch of new reports to coverage -- methodcoverage, symbolcoverage, branchcoverage, and cyclomaticcomplexity on top of the other reports brought in tasks definition file to go along with msbuild extensions renamed MSBuild.ExtensionPack tools directory added in MSBuild.Mercurial extension tasks added in missing html help dll

--- EPS.Utility/Properties/AssemblyInfo.cs
+++ EPS.Utility/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EPS.Utility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("EPS.Utility")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +19,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("34a5780f-8fc9-49d4-90c4-67d866e3c693")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
