c97b1c21436e635207646687cae2d22d128f04cb
ebrown <devnull@localhost>
2011-01-23 20:19:22 -0500

    CustomDictionary.xml has been added to all projects as a linked CodeAnalysisDictionary file -- so that abbreviations like EPS are ignored removed all suppressions for CA1709 as they pertain to 'EPS' as the custom dictionary now handles this

--- EPS.Utility/Properties/AssemblyInfo.cs
+++ EPS.Utility/Properties/AssemblyInfo.cs
@@ -18,28 +18,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("34a5780f-8fc9-49d4-90c4-67d866e3c693")]
 
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Utility", Scope = "assembly", Justification = "EPS is a proper company name")]
-
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Collections", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Configuration", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Configuration.Abstractions", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Conversions", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Diagnostics", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Dynamic", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.IO", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Linq", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Linq.Expressions", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Net", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Reflection", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Reflection.Emit", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Runtime.Serialization.Json", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Security", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Security.Cryptography", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Text", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Threading", Scope = "namespace", Justification = "EPS is a proper company name")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", Target = "EPS.Windows.Security", Scope = "namespace", Justification = "EPS is a proper company name")]
-
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Target = "EPS", Scope = "namespace", Justification = "Helpers mirror .NET framework type layout")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Target = "EPS.Collections", Scope = "namespace", Justification = "Helpers mirror .NET framework type layout")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Target = "EPS.Conversions", Scope = "namespace", Justification = "Helpers mirror .NET framework type layout")]
