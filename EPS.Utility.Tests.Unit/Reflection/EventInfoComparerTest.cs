ecfb55dfda96fee5b569ae5146c3fb495945cf63
ebrown <devnull@localhost>
2011-02-03 17:48:43 -0500

    a couple more quick tests

--- EPS.Utility.Tests.Unit/Reflection/EventInfoComparerTest.cs
+++ EPS.Utility.Tests.Unit/Reflection/EventInfoComparerTest.cs
@@ -1,25 +1,40 @@
 using System;
 using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace EPS.Reflection.Tests.Unit
 {
     public class EventInfoComparerTest
     {
-        class TypeA
+        class A
         {
             public event EventHandler Test;
+            public event EventHandler<UnhandledExceptionEventArgs> Test2;
         }
 
-        class TypeB
+        class B
         {
             public event EventHandler Test;
+            public event EventHandler<UnhandledExceptionEventArgs> Test2;
         }
 
         [Fact]
         public void Equals_True_OnTypesOfSameSignature()
         {
-            Assert.True(typeof(TypeA).GetEvents().SequenceEqual(typeof(TypeB).GetEvents(), EventInfoComparer.Default));
+            Assert.True(typeof(A).GetEvents().SequenceEqual(typeof(B).GetEvents(), EventInfoComparer.Default));
+        }
+
+        [Fact]
+        public void Equals_False_OnNullFirstParameter()
+        {
+            Assert.False(EventInfoComparer.Default.Equals(null, (EventInfo)typeof(A).GetMember("Test")[0]));
+        }
+
+        [Fact]
+        public void Equals_False_OnNullSecondParameter()
+        {
+            Assert.False(EventInfoComparer.Default.Equals((EventInfo)typeof(A).GetMember("Test")[0], null));
         }
     }
 }
