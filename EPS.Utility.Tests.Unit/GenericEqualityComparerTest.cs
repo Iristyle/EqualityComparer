6f4be7bc275167f09cf3ab9fe9a64656f47e2eab
ebrown <devnull@localhost>
2011-06-06 22:38:39 -0400

    Wrote docs for .ByAllProperties wrote additional tests to cover class

--- EPS.Utility.Tests.Unit/GenericEqualityComparerTest.cs
+++ EPS.Utility.Tests.Unit/GenericEqualityComparerTest.cs
@@ -4,59 +4,77 @@ using Xunit;
 
 namespace EPS.Utility.Tests.Unit
 {
-    public class GenericEqualityComparerTest
-    {
-        class A
-        {
-            public A(int integer, string @string)
-            {
-                Integer = integer;
-                String = @string;
-            }
+	public class GenericEqualityComparerTest
+	{
+		class A
+		{
+			public A(int integer, string @string)
+			{
+				Integer = integer;
+				String = @string;
+			}
 
-            public int Integer { get; set; }
-            public string String { get; set; }
+			public int Integer { get; set; }
+			public string String { get; set; }
 
-            public static GenericEqualityComparer<A> IntegerOnlyComparer = new GenericEqualityComparer<A>((a1, a2) => a1.Integer == a2.Integer);
-            public static GenericEqualityComparer<A> AllPropertiesComparer = new GenericEqualityComparer<A>((a1, a2) => a1.Integer == a2.Integer && a1.String == a2.String);
-        }
+			public static GenericEqualityComparer<A> IntegerOnlyComparer = new GenericEqualityComparer<A>((a1, a2) => a1.Integer == a2.Integer);
+			public static GenericEqualityComparer<A> AllPropertiesComparer = new GenericEqualityComparer<A>((a1, a2) => a1.Integer == a2.Integer && a1.String == a2.String);
+		}
 
-        [Fact]
-        [SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Justification = "We're testing the constructor!")]
-        public void Constructor_ThrowsOnNullFunc()
-        {
-            Assert.Throws<ArgumentNullException>(() => { var comparer = new GenericEqualityComparer<A>(null); });
-        }
+		[Fact]
+		[SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Justification = "We're testing the constructor!")]
+		public void Constructor_ThrowsOnNullFunc()
+		{
+			Assert.Throws<ArgumentNullException>(() => { var comparer = new GenericEqualityComparer<A>(null); });
+		}
 
-        [Fact]
-        [SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Justification = "We're testing the constructor!")]
-        public void Constructor_ThrowsOnNullHashGenerator()
-        {
-            Assert.Throws<ArgumentNullException>(() => { var comparer = new GenericEqualityComparer<A>((a,b) => a.Integer == b.Integer, null); });
-        }
+		[Fact]
+		[SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Justification = "We're testing the constructor!")]
+		public void Constructor_ThrowsOnNullHashGenerator()
+		{
+			Assert.Throws<ArgumentNullException>(() => { var comparer = new GenericEqualityComparer<A>((a, b) => a.Integer == b.Integer, null); });
+		}
 
-        [Fact]
-        public void Compare_TrueOnMatchingObjectsWithSpecifiedPropertiesOnly()
-        {
-            A a = new A(1, "foo"),
-                b = new A(1, "bar");
-            Assert.Equal(a, b, A.IntegerOnlyComparer);
-        }
+		[Fact]
+		public void Compare_TrueOnMatchingObjectsWithSpecifiedPropertiesOnly()
+		{
+			A a = new A(1, "foo"),
+				b = new A(1, "bar");
+			Assert.Equal(a, b, A.IntegerOnlyComparer);
+		}
 
-        [Fact]
-        public void Compare_TrueOnMatchingObjectsWithMultipleProperties()
-        {
-            A a = new A(1, "string"), 
-                b = new A(a.Integer, a.String);
-            Assert.Equal(a, b, A.AllPropertiesComparer);
-        }
+		[Fact]
+		public void Compare_TrueOnMatchingObjectsWithMultipleProperties()
+		{
+			A a = new A(1, "string"),
+				b = new A(a.Integer, a.String);
+			Assert.Equal(a, b, A.AllPropertiesComparer);
+		}
 
-        [Fact]
-        public void Compare_FalseOnMismatchedObjects()
-        {
-            A a = new A(1, string.Empty), 
-                b = new A(2, string.Empty);
-            Assert.NotEqual(a, b, A.IntegerOnlyComparer);
-        }
-    }
+		[Fact]
+		public void Compare_FalseOnMismatchedObjects()
+		{
+			A a = new A(1, string.Empty),
+				b = new A(2, string.Empty);
+			Assert.NotEqual(a, b, A.IntegerOnlyComparer);
+		}
+
+		[Fact]
+		public void ByAllProperties_TrueOnMatchedObjectInstances()
+		{
+			A a = new A(3, "Foo"),
+			b = new A(3, "Foo");
+
+			Assert.Equal(a, b, GenericEqualityComparer<A>.ByAllProperties());
+		}
+
+		[Fact]
+		public void ByAllProperties_FalseOnUnMatchedObjectInstances()
+		{
+			A a = new A(5, "Foo"),
+			b = new A(3, "Bar");
+
+			Assert.NotEqual(a, b, GenericEqualityComparer<A>.ByAllProperties());
+		}
+	}
 }
--- EPS.Utility/GenericEqualityComparer.cs
+++ EPS.Utility/GenericEqualityComparer.cs
@@ -3,59 +3,62 @@ using System.Collections.Generic;
 
 namespace EPS.Utility
 {
-    /// <summary>   A generic comparer that takes aceepts a Func{T, T, bool} to create simple on-the-fly comparison routines. </summary>
-    /// <remarks>   ebrown, 2/7/2011. </remarks>
-    public class GenericEqualityComparer<T> : IEqualityComparer<T>
-    {
-        private readonly Func<T, T, bool> _comparer;
-        private readonly Func<T, int> _hasher;
+	/// <summary>   A generic comparer that takes aceepts a Func{T, T, bool} to create simple on-the-fly comparison routines. </summary>
+	/// <remarks>   ebrown, 2/7/2011. </remarks>
+	public class GenericEqualityComparer<T> : IEqualityComparer<T>
+	{
+		private readonly Func<T, T, bool> _comparer;
+		private readonly Func<T, int> _hasher;
 
-        /// <summary>   Constructor accepting the comparison function. </summary>
-        /// <remarks>   Uses a hasher function that always returns 0 for given instances.  Don't use this for any sorting operations. </remarks>
-        /// <exception cref="ArgumentNullException">    Thrown when the comparer or hashers are null. </exception>
-        /// <param name="comparer">   The comparison function to use when comparing the two instances. </param>
-        public GenericEqualityComparer(Func<T, T, bool> comparer) :
-            this(comparer, o => 0)
-        { }
+		/// <summary>   Constructor accepting the comparison function. </summary>
+		/// <remarks>   Uses a hasher function that always returns 0 for given instances.  Don't use this for any sorting operations. </remarks>
+		/// <exception cref="ArgumentNullException">    Thrown when the comparer or hashers are null. </exception>
+		/// <param name="comparer">   The comparison function to use when comparing the two instances. </param>
+		public GenericEqualityComparer(Func<T, T, bool> comparer) :
+			this(comparer, o => 0)
+		{ }
 
-        /// <summary>   Constructor accepting the comparison function and hashing function. </summary>
-        /// <remarks>   ebrown, 2/7/2011. </remarks>
-        /// <exception cref="ArgumentNullException">    Thrown when the comparer or hashers are null. </exception>
-        /// <param name="comparer">   The comparison function to use when comparing the two instances. </param>
-        /// <param name="hasher">       The hash function used to generate object hashes on the instances. </param>
-        public GenericEqualityComparer(Func<T, T, bool> comparer, Func<T, int> hasher)
-        {
-            if (comparer == null)
-                throw new ArgumentNullException("comparer");
-            if (hasher == null)
-                throw new ArgumentNullException("hasher");
+		/// <summary>   Constructor accepting the comparison function and hashing function. </summary>
+		/// <remarks>   ebrown, 2/7/2011. </remarks>
+		/// <exception cref="ArgumentNullException">    Thrown when the comparer or hashers are null. </exception>
+		/// <param name="comparer">   The comparison function to use when comparing the two instances. </param>
+		/// <param name="hasher">       The hash function used to generate object hashes on the instances. </param>
+		public GenericEqualityComparer(Func<T, T, bool> comparer, Func<T, int> hasher)
+		{
+			if (comparer == null)
+				throw new ArgumentNullException("comparer");
+			if (hasher == null)
+				throw new ArgumentNullException("hasher");
 
-            this._comparer = comparer;
-            this._hasher = hasher;
-        }
+			this._comparer = comparer;
+			this._hasher = hasher;
+		}
 
-        /// <summary>   Tests if two T objects are considered equal. </summary>
-        /// <remarks>   Uses the passed in Func{T, T, bool} for the comparison. </remarks>
-        /// <param name="x">    T to be compared. </param>
-        /// <param name="y">    T to be compared. </param>
-        /// <returns>   true if the objects are considered equal, false if they are not. </returns>
-        public bool Equals(T x, T y)
-        {
-            return this._comparer(x, y);
-        }
+		/// <summary>   Tests if two T objects are considered equal. </summary>
+		/// <remarks>   Uses the passed in Func{T, T, bool} for the comparison. </remarks>
+		/// <param name="x">    T to be compared. </param>
+		/// <param name="y">    T to be compared. </param>
+		/// <returns>   true if the objects are considered equal, false if they are not. </returns>
+		public bool Equals(T x, T y)
+		{
+			return this._comparer(x, y);
+		}
 
-        /// <summary>   Calculates the hash code for this object. </summary>
-        /// <remarks>   If no hasher function was supplied, will always return 0.  Otherwise uses Func{T, int} hasher function supplied. </remarks>
-        /// <param name="obj">  The object. </param>
-        /// <returns>   The hash code for this object. </returns>
-        public int GetHashCode(T obj)
-        {
-            return this._hasher(obj);
-        }
+		/// <summary>   Calculates the hash code for this object. </summary>
+		/// <remarks>   If no hasher function was supplied, will always return 0.  Otherwise uses Func{T, int} hasher function supplied. </remarks>
+		/// <param name="obj">  The object. </param>
+		/// <returns>   The hash code for this object. </returns>
+		public int GetHashCode(T obj)
+		{
+			return this._hasher(obj);
+		}
 
+		/// <summary>	Shortcut method to get a simple generic IEqualityComparer{T} where the comparison is by all properties on the instance. </summary>
+		/// <remarks>	ebrown, 6/6/2011. </remarks>
+		/// <returns>	A GenericEqualityComparer{T}. </returns>
 		public static GenericEqualityComparer<T> ByAllProperties()
 		{
 			return new GenericEqualityComparer<T>((x, y) => PropertyComparer.Equal(x, y));
 		}
-    }
+	}
 }
