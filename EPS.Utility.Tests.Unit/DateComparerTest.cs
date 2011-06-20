ac72da6e80dc49eb2a629ab3fcb404a842653d26
Don McNamara <dmcnamara@eastpointsystems.com>
2011-06-20 13:40:29 -0400

    Adding unit test for DateComparer. One failing test right now until we figure out how the comparer should behave.

---
+++ EPS.Utility.Tests.Unit/DateComparerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EPS.Utility.Tests.Unit
+{
+	public class DateComparerTest
+	{
+
+		[Fact]
+		public void ExactComparison_SameDateTime_ReturnsTrue()
+		{
+			DateTime testValue = new DateTime(2011, 6, 20, 13, 30, 1, 200);
+			Assert.True(new DateComparer(DateComparisonType.Exact).Equals(testValue, testValue));
+		}
+
+		[Fact]
+		public void ExactComparison_DifferentDateTime_ReturnsFalse()
+		{
+			DateTime testValue = new DateTime(2011, 6, 20, 13, 30, 1, 200);
+			Assert.False(new DateComparer(DateComparisonType.Exact).Equals(testValue, testValue.AddMilliseconds(100)));
+		}
+
+		[Fact]
+		public void ToTheSecondComparison_SlightlyDifferentDateTime_ReturnsTrue()
+		{
+			DateTime testValue = new DateTime(2011, 6, 20, 13, 30, 1, 200);
+			Assert.True(new DateComparer(DateComparisonType.ToSecond).Equals(testValue, testValue.AddMilliseconds(200)));
+		}
+
+		[Fact]
+		public void ToTheSecondComparison_SlightlyDifferentDateTimeWithDifferentSecondValues_ReturnsTrue()
+		{
+			// note: 900ms + 200ms diff causes the second value to increment.
+			DateTime testValue = new DateTime(2011, 6, 20, 13, 30, 1, 900);
+
+			// TODO: should this return true or false?
+			Assert.True(new DateComparer(DateComparisonType.ToSecond).Equals(testValue, testValue.AddMilliseconds(200)));
+		}
+
+		[Fact]
+		public void ToTheSecondComparison_DateTimeWithDifferenceGreaterThanOneSecond_ReturnsFalse()
+		{
+			DateTime testValue = new DateTime(2011, 6, 20, 13, 30, 1, 900);
+			Assert.False(new DateComparer(DateComparisonType.ToSecond).Equals(testValue, testValue.AddMilliseconds(1100)));
+		}
+
+	}
+}
